80d9c1002eed644b5c6b487c318f287827276a3c
IzanDuesoLopez <izandueso@gmail.com>
2022-08-16 13:20:25 +0200

    Ejercicio 2 - Service

---
+++ Ejercicio_2_Los_cientificos/src/main/java/com/example/demo/service/CientificoServiceImpl.java
@@ -0,0 +1,48 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.dao.ICientificoDAO;
+import com.example.demo.dto.Cientifico;
+
+@Service
+public class CientificoServiceImpl implements ICientificoService{
+
+	// Utilizamos metodos de la interfaz
+	@Autowired
+	ICientificoDAO iCientificoDAO;
+	
+	@Override
+	public List<Cientifico> listarCientificos() {
+		return iCientificoDAO.findAll();
+	}
+
+	@Override
+	public Cientifico guardarCientifico(Cientifico cientifico) {
+		return iCientificoDAO.save(cientifico);
+	}
+
+	@Override
+	public Cientifico cientificoXDni(String dni) {
+		return iCientificoDAO.findById(dni).get();
+	}
+
+	@Override
+	public List<Cientifico> listarCientificosNombre(String nombre) {
+		return iCientificoDAO.findByNombre(nombre);
+	}
+
+	@Override
+	public Cientifico actualizarCientifico(Cientifico cientifico) {
+		return iCientificoDAO.save(cientifico);
+	}
+
+	@Override
+	public void eliminarCientifico(String dni) {
+		iCientificoDAO.deleteById(dni);
+	}
+
+}
---
+++ Ejercicio_2_Los_cientificos/src/main/java/com/example/demo/service/ICientificoService.java
@@ -0,0 +1,16 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import com.example.demo.dto.Cientifico;
+public interface ICientificoService {
+
+	// Metodos del crud cientifico
+	public List<Cientifico> listarCientificos();
+	public Cientifico guardarCientifico(Cientifico cientifico);
+	public Cientifico cientificoXDni(String dni);
+	public List<Cientifico> listarCientificosNombre(String nombre);
+	public Cientifico actualizarCientifico(Cientifico cientifico);
+	public void eliminarCientifico(String dni);
+	
+}
