b872175ad14088c9ec865aa7d059a12a2f5f92d4
IzanDuesoLopez <izandueso@gmail.com>
2022-08-16 11:04:20 +0200

    Ejercicio 1 - Piezas y Proveedores

--- Ejercicio2_Empleados-1/src/main/java/com/example/demo/dao/ISuministraDAO.java
+++ Ejercicio2_Empleados-1/src/main/java/com/example/demo/dao/ISuministraDAO.java
@@ -7,8 +7,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.dto.Suministra;
 
 public interface ISuministraDAO extends JpaRepository<Suministra, Integer>{
-
-	// Suministra por id
-	public List<Suministra> findByNombre(int id);
-	
 }
