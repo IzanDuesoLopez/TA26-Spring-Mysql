debaf04d6674ce7831b72d2b137ed0aaa8d29e44
IzanDuesoLopez <izandueso@gmail.com>
2022-08-16 12:18:31 +0200

    Ejercicio 2 - Cientificos - Inicio

---
+++ Ejercicio_2_Los_cientificos/src/main/resources/data.sql
@@ -0,0 +1,30 @@
+use cientificos;
+drop table if exists cientificos, proyectos, asignado_a;
+
+create table cientificos (
+	dni varchar(8),
+    nom_apels nvarchar(255),
+    primary key (dni)
+);
+
+create table proyectos (
+	id char(4),
+    nombre nvarchar(255),
+    horas int,
+    primary key (id)
+);
+
+create table asignado_a (
+	cientifico varchar(8),
+    proyecto char(4),
+    primary key (cientifico, proyecto),
+    foreign key (cientifico) references cientificos (dni)
+    on delete cascade on update cascade,
+    foreign key (proyecto) references proyectos (id)
+    on delete cascade on update cascade
+);
+
+
+insert into cientificos(dni, nom_apels)values('12345678', 'Izan Dueso');
+insert into proyectos(id, nombre, horas)values('aaaa', 'Spring', 300);
+insert into asignado_a(cientifico, proyecto)values('12345678', 'aaaa');
