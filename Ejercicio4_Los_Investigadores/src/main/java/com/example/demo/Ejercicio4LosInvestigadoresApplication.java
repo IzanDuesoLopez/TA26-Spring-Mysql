1f920cd4ff9bd37c4f23835238181684cbd8c615
IzanDuesoLopez <izandueso@gmail.com>
2022-08-21 21:44:19 +0200

    Commit inicial

---
+++ Ejercicio4_Los_Investigadores/src/main/java/com/example/demo/Ejercicio4LosInvestigadoresApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejercicio4LosInvestigadoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejercicio4LosInvestigadoresApplication.class, args);
+	}
+
+}
